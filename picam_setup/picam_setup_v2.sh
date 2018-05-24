a636ecdd7c314e7e018c5b1573fd30cf5270999c
matdoess <matthias.doess@gmail.com>
2018-05-24 19:46:40 +0200

    setup dateien aktuallisiert

--- picam_setup/picam_setup_v2.sh
+++ picam_setup/picam_setup_v2.sh
@@ -14,4 +14,5 @@ alsamixer
 # Install picam binary
 wget https://github.com/iizukanao/picam/releases/download/v1.4.6/picam-1.4.6-binary-stretch.tar.xz
 tar xvf picam-1.4.6-binary-stretch.tar.xz
+mkdir ~/picam
 cp picam-1.4.6-binary-stretch/picam ~/picam/
