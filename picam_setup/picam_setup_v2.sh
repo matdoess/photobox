45fa334af1fb2f0df4039b2cc1ded8ccbe6a9d6d
matdoess <matthias.doess@gmail.com>
2018-05-24 19:36:16 +0200

    setup dateien aktuallisiert

---
+++ picam_setup/picam_setup_v2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# If you have not enabled camera, enable it with raspi-config then reboot
+#sudo raspi-config
+
+# Install dependencies
+sudo apt-get update
+sudo apt-get install libharfbuzz0b libfontconfig1
+
+# Optionally, increase microphone volume with alsamixer
+read -p "Optionally, increase microphone volume with alsamixer. Press ENTER to continue."
+alsamixer
+
+# Install picam binary
+wget https://github.com/iizukanao/picam/releases/download/v1.4.6/picam-1.4.6-binary-stretch.tar.xz
+tar xvf picam-1.4.6-binary-stretch.tar.xz
+cp picam-1.4.6-binary-stretch/picam ~/picam/
