3240039adb0d2965b3f0cb845a5d734d7902874b
matdoess <matthias.doess@gmail.com>
2018-06-01 02:55:59 +0200

    Backupscript optimiert

--- script/backup_gitignorefiles.sh
+++ script/backup_gitignorefiles.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -av --progress --exclude-from=backup_gitignorefiles.list /home/pi/photobox /home/pi/photobox_storage_backup/
+rsync -av --progress --exclude-from=/home/pi/photobox/script/backup_gitignorefiles.list /home/pi/photobox /home/pi/photobox_storage_backup/
